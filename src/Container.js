a84cb2dedb5249501a2ddfc6b6d5071380b6f0c0
Quynh-Chi Nguyen <quynh-chi.nguyen@stileeducation.com>
2019-03-07 17:10:26 +1100

    Hack in horizontal sticky compatibility

--- src/Container.js
+++ src/Container.js
@@ -3,91 +3,103 @@ import PropTypes from "prop-types";
 import raf from "raf";
 
 export default class Container extends PureComponent {
-  static childContextTypes = {
-    subscribe: PropTypes.func,
-    unsubscribe: PropTypes.func,
-    getParent: PropTypes.func
-  };
-
-  getChildContext() {
-    return {
-      subscribe: this.subscribe,
-      unsubscribe: this.unsubscribe,
-      getParent: this.getParent
+    static childContextTypes = {
+        subscribe: PropTypes.func,
+        unsubscribe: PropTypes.func,
+        getParent: PropTypes.func,
     };
-  }
 
-  events = [
-    "resize",
-    "scroll",
-    "touchstart",
-    "touchmove",
-    "touchend",
-    "pageshow",
-    "load"
-  ];
+    getChildContext() {
+        return {
+            subscribe: this.subscribe,
+            unsubscribe: this.unsubscribe,
+            getParent: this.getParent,
+        };
+    }
 
-  subscribers = [];
+    events = [
+        "resize",
+        "scroll",
+        "touchstart",
+        "touchmove",
+        "touchend",
+        "pageshow",
+        "load",
+    ];
 
-  rafHandle = null;
+    subscribers = [];
 
-  subscribe = handler => {
-    this.subscribers = this.subscribers.concat(handler);
-  };
+    rafHandle = null;
 
-  unsubscribe = handler => {
-    this.subscribers = this.subscribers.filter(current => current !== handler);
-  };
+    subscribe = handler => {
+        this.subscribers = this.subscribers.concat(handler);
+    };
 
-  notifySubscribers = evt => {
-    if (!this.framePending) {
-      const { currentTarget } = evt;
-
-      this.rafHandle = raf(() => {
-        this.framePending = false;
-        const { top, bottom } = this.node.getBoundingClientRect();
-
-        this.subscribers.forEach(handler =>
-          handler({
-            distanceFromTop: top,
-            distanceFromBottom: bottom,
-            eventSource: currentTarget === window ? document.body : this.node
-          })
+    unsubscribe = handler => {
+        this.subscribers = this.subscribers.filter(
+            current => current !== handler,
         );
-      });
-      this.framePending = true;
+    };
+
+    notifySubscribers = evt => {
+        if (!this.framePending) {
+            const { currentTarget } = evt;
+
+            this.rafHandle = raf(() => {
+                this.framePending = false;
+                const {
+                    top,
+                    left,
+                    bottom,
+                    right,
+                } = this.node.getBoundingClientRect();
+
+                this.subscribers.forEach(handler =>
+                    handler({
+                        distanceFromTop: top,
+                        distanceFromBottom: bottom,
+                        distanceFromLeft: left,
+                        distanceFromRight: right,
+                        eventSource:
+                            currentTarget === window
+                                ? document.body
+                                : this.node,
+                    }),
+                );
+            });
+            this.framePending = true;
+        }
+    };
+
+    getParent = () => this.node;
+
+    componentDidMount() {
+        this.events.forEach(event =>
+            window.addEventListener(event, this.notifySubscribers),
+        );
     }
-  };
 
-  getParent = () => this.node;
+    componentWillUnmount() {
+        if (this.rafHandle) {
+            raf.cancel(this.rafHandle);
+            this.rafHandle = null;
+        }
 
-  componentDidMount() {
-    this.events.forEach(event =>
-      window.addEventListener(event, this.notifySubscribers)
-    );
-  }
-
-  componentWillUnmount() {
-    if (this.rafHandle) {
-      raf.cancel(this.rafHandle);
-      this.rafHandle = null;
+        this.events.forEach(event =>
+            window.removeEventListener(event, this.notifySubscribers),
+        );
     }
 
-    this.events.forEach(event =>
-      window.removeEventListener(event, this.notifySubscribers)
-    );
-  }
-
-  render() {
-    return (
-      <div
-        {...this.props}
-        ref={node => (this.node = node)}
-        onScroll={this.notifySubscribers}
-        onTouchStart={this.notifySubscribers}
-        onTouchMove={this.notifySubscribers}
-        onTouchEnd={this.notifySubscribers}
-      />
-    );
-  }
+    render() {
+        return (
+            <div
+                {...this.props}
+                ref={node => (this.node = node)}
+                onScroll={this.notifySubscribers}
+                onTouchStart={this.notifySubscribers}
+                onTouchMove={this.notifySubscribers}
+                onTouchEnd={this.notifySubscribers}
+            />
+        );
+    }
 }
--- src/Sticky.js
+++ src/Sticky.js
@@ -3,135 +3,199 @@ import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 
 export default class Sticky extends Component {
-  static propTypes = {
-    topOffset: PropTypes.number,
-    bottomOffset: PropTypes.number,
-    relative: PropTypes.bool,
-    children: PropTypes.func.isRequired
-  };
+    // Set horizontalSticky to stick horizontally, otherwise it'll stick
+    // vertically
+    static propTypes = {
+        topOffset: PropTypes.number,
+        bottomOffset: PropTypes.number,
+        leftOffset: PropTypes.number,
+        rightOffset: PropTypes.number,
+        horizontalSticky: PropTypes.bool,
+        relative: PropTypes.bool,
+        children: PropTypes.func.isRequired,
+    };
 
-  static defaultProps = {
-    relative: false,
-    topOffset: 0,
-    bottomOffset: 0,
-    disableCompensation: false,
-    disableHardwareAcceleration: false
-  };
+    static defaultProps = {
+        relative: false,
+        topOffset: 0,
+        bottomOffset: 0,
+        leftOffset: 0,
+        rightOffset: 0,
+        horizontalSticky: false,
+        disableCompensation: false,
+        disableHardwareAcceleration: false,
+    };
 
-  static contextTypes = {
-    subscribe: PropTypes.func,
-    unsubscribe: PropTypes.func,
-    getParent: PropTypes.func
-  };
+    static contextTypes = {
+        subscribe: PropTypes.func,
+        unsubscribe: PropTypes.func,
+        getParent: PropTypes.func,
+    };
 
-  state = {
-    isSticky: false,
-    wasSticky: false,
-    style: {}
-  };
+    state = {
+        isSticky: false,
+        wasSticky: false,
+        style: {},
+    };
 
-  componentWillMount() {
-    if (!this.context.subscribe)
-      throw new TypeError(
-        "Expected Sticky to be mounted within StickyContainer"
-      );
+    componentWillMount() {
+        if (!this.context.subscribe)
+            throw new TypeError(
+                "Expected Sticky to be mounted within StickyContainer",
+            );
 
-    this.context.subscribe(this.handleContainerEvent);
-  }
-
-  componentWillUnmount() {
-    this.context.unsubscribe(this.handleContainerEvent);
-  }
-
-  componentDidUpdate() {
-    this.placeholder.style.paddingBottom = this.props.disableCompensation
-      ? 0
-      : `${this.state.isSticky ? this.state.calculatedHeight : 0}px`;
-  }
-
-  handleContainerEvent = ({
-    distanceFromTop,
-    distanceFromBottom,
-    eventSource
-  }) => {
-    const parent = this.context.getParent();
-
-    let preventingStickyStateChanges = false;
-    if (this.props.relative) {
-      preventingStickyStateChanges = eventSource !== parent;
-      distanceFromTop =
-        -(eventSource.scrollTop + eventSource.offsetTop) +
-        this.placeholder.offsetTop;
+        this.context.subscribe(this.handleContainerEvent);
     }
 
-    const placeholderClientRect = this.placeholder.getBoundingClientRect();
-    const contentClientRect = this.content.getBoundingClientRect();
-    const calculatedHeight = contentClientRect.height;
-
-    const bottomDifference =
-      distanceFromBottom - this.props.bottomOffset - calculatedHeight;
-
-    const wasSticky = !!this.state.isSticky;
-    const isSticky = preventingStickyStateChanges
-      ? wasSticky
-      : distanceFromTop <= -this.props.topOffset &&
-        distanceFromBottom > -this.props.bottomOffset;
-
-    distanceFromBottom =
-      (this.props.relative
-        ? parent.scrollHeight - parent.scrollTop
-        : distanceFromBottom) - calculatedHeight;
-
-    const style = !isSticky
-      ? {}
-      : {
-          position: "fixed",
-          top:
-            bottomDifference > 0
-              ? this.props.relative
-                ? parent.offsetTop - parent.offsetParent.scrollTop
-                : 0
-              : bottomDifference,
-          left: placeholderClientRect.left,
-          width: placeholderClientRect.width
-        };
-
-    if (!this.props.disableHardwareAcceleration) {
-      style.transform = "translateZ(0)";
+    componentWillUnmount() {
+        this.context.unsubscribe(this.handleContainerEvent);
     }
 
-    this.setState({
-      isSticky,
-      wasSticky,
-      distanceFromTop,
-      distanceFromBottom,
-      calculatedHeight,
-      style
-    });
-  };
-
-  render() {
-    const element = React.cloneElement(
-      this.props.children({
-        isSticky: this.state.isSticky,
-        wasSticky: this.state.wasSticky,
-        distanceFromTop: this.state.distanceFromTop,
-        distanceFromBottom: this.state.distanceFromBottom,
-        calculatedHeight: this.state.calculatedHeight,
-        style: this.state.style
-      }),
-      {
-        ref: content => {
-          this.content = ReactDOM.findDOMNode(content);
+    componentDidUpdate() {
+        if (this.props.horizontalSticky) {
+            this.placeholder.style.paddingRight = this.props.disableCompensation
+                ? 0
+                : `${this.state.isSticky ? this.state.calculatedWidth : 0}px`;
+        } else {
+            this.placeholder.style.paddingBottom = this.props
+                .disableCompensation
+                ? 0
+                : `${this.state.isSticky ? this.state.calculatedHeight : 0}px`;
         }
-      }
-    );
+    }
 
-    return (
-      <div>
-        <div ref={placeholder => (this.placeholder = placeholder)} />
-        {element}
-      </div>
-    );
-  }
+    handleContainerEvent = ({
+        distanceFromTop,
+        distanceFromBottom,
+        distanceFromLeft,
+        distanceFromRight,
+        eventSource,
+    }) => {
+        const parent = this.context.getParent();
+
+        let preventingStickyStateChanges = false;
+        if (this.props.relative) {
+            preventingStickyStateChanges = eventSource !== parent;
+            distanceFromTop =
+                -(eventSource.scrollTop + eventSource.offsetTop) +
+                this.placeholder.offsetTop;
+            distanceFromLeft =
+                -(eventSource.scrollLeft + eventSource.offsetLeft) +
+                this.placeholder.offsetLeft;
+        }
+
+        const placeholderClientRect = this.placeholder.getBoundingClientRect();
+        const contentClientRect = this.content.getBoundingClientRect();
+        const calculatedHeight = contentClientRect.height;
+        const calculatedWidth = contentClientRect.width;
+
+        const bottomDifference =
+            distanceFromBottom - this.props.bottomOffset - calculatedHeight;
+
+        const rightDifference =
+            distanceFromRight - this.props.rightOffset - calculatedWidth;
+
+        const wasSticky = !!this.state.isSticky;
+        let isSticky;
+        if (this.props.horizontalSticky) {
+            isSticky = preventingStickyStateChanges
+                ? wasSticky
+                : distanceFromLeft <= -this.props.leftOffset &&
+                  distanceFromRight > -this.props.rightOffset;
+        } else {
+            isSticky = preventingStickyStateChanges
+                ? wasSticky
+                : distanceFromTop <= -this.props.topOffset &&
+                  distanceFromBottom > -this.props.bottomOffset;
+        }
+
+        distanceFromBottom =
+            (this.props.relative
+                ? parent.scrollHeight - parent.scrollTop
+                : distanceFromBottom) - calculatedHeight;
+
+        distanceFromRight =
+            (this.props.relative
+                ? parent.scrollWidth - parent.scrollLeft
+                : distanceFromRight) - calculatedWidth;
+
+        const style = !isSticky
+            ? {}
+            : {
+                  position: "fixed",
+                  top: this.props.horizontalSticky
+                      ? placeholderClientRect.top
+                      : bottomDifference > 0
+                      ? this.props.relative
+                          ? parent.offsetTop - parent.offsetParent.scrollTop
+                          : 0
+                      : bottomDifference,
+                  left: !this.props.horizontalSticky
+                      ? placeholderClientRect.left
+                      : rightDifference > 0
+                      ? this.props.relative
+                          ? parent.offsetLeft - parent.offsetParent.scrollLeft
+                          : 0
+                      : rightDifference,
+                  width: placeholderClientRect.width,
+                  height: placeholderClientRect.height,
+              };
+
+        if (!this.props.disableHardwareAcceleration) {
+            style.transform = "translateZ(0)";
+        }
+
+        this.setState({
+            isSticky,
+            wasSticky,
+            distanceFromTop,
+            distanceFromBottom,
+            calculatedHeight,
+            distanceFromLeft,
+            distanceFromRight,
+            calculatedWidth,
+            style,
+        });
+    };
+
+    render() {
+        const element = React.cloneElement(
+            this.props.children({
+                isSticky: this.state.isSticky,
+                wasSticky: this.state.wasSticky,
+                distanceFromTop: this.state.distanceFromTop,
+                distanceFromBottom: this.state.distanceFromBottom,
+                distanceFromLeft: this.state.distanceFromLeft,
+                distanceFromRight: this.state.distanceFromRight,
+                calculatedHeight: this.state.calculatedHeight,
+                calculatedWidth: this.state.calculatedWidth,
+                style: this.state.style,
+            }),
+            {
+                ref: content => {
+                    this.content = ReactDOM.findDOMNode(content);
+                },
+            },
+        );
+
+        return (
+            <div
+                style={{
+                    display: this.props.horizontalSticky
+                        ? "inline-block"
+                        : "inline",
+                }}
+            >
+                <div
+                    ref={placeholder => (this.placeholder = placeholder)}
+                    style={{
+                        display: this.props.horizontalSticky
+                            ? "inline-block"
+                            : "inline",
+                    }}
+                />
+                {element}
+            </div>
+        );
+    }
 }
