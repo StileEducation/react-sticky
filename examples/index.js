29c62452af6797fc3e0bb1cf2f08e7d8cdcac5e4
Quynh-Chi Nguyen <quynh-chi.nguyen@stileeducation.com>
2019-03-08 18:18:48 +1100

    Add example for horizontal sticky

--- examples/index.js
+++ examples/index.js
@@ -4,21 +4,23 @@ import { HashRouter as Router, Route, Redirect } from "react-router-dom";
 import { Basic } from "./basic/basic";
 import { Relative } from "./relative/relative";
 import { Stacked } from "./stacked/stacked";
+import { Horizontal } from "./horizontal/horizontal";
 import { Navbar } from "./navbar";
 import styles from "./styles";
 
 ReactDOM.render(
-  <Router>
-    <div className="app">
-      <style>{styles}</style>
-      <Navbar />
-      <div className="examples">
-        <Route exact path="/" render={() => <Redirect to="/basic" />} />
-        <Route path="/basic" component={Basic} />
-        <Route path="/relative" component={Relative} />
-        <Route path="/stacked" component={Stacked} />
-      </div>
-    </div>
-  </Router>,
-  document.querySelector("#mount")
+    <Router>
+        <div className="app">
+            <style>{styles}</style>
+            <Navbar />
+            <div className="examples">
+                <Route exact path="/" render={() => <Redirect to="/basic" />} />
+                <Route path="/basic" component={Basic} />
+                <Route path="/relative" component={Relative} />
+                <Route path="/stacked" component={Stacked} />
+                <Route path="/horizontal" component={Horizontal} />
+            </div>
+        </div>
+    </Router>,
+    document.querySelector("#mount"),
 );
